71036b8bf7bf72134b1ad70e8ed8b3a5ba798db5
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2025-02-02 14:58:36 +0200

    fix: imports

--- src/main/java/mz/co/muianga/productapi/model/Product.java
+++ src/main/java/mz/co/muianga/productapi/model/Product.java
@@ -1,11 +1,14 @@
 package mz.co.muianga.productapi.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-import mz.co.muianga.productapi.converter.DTOConverter;
-import mz.co.muianga.productapi.dto.CategoryDTO;
-import mz.co.muianga.productapi.dto.ProductDTO;
 
 @Entity(name = "product")
 @Getter
