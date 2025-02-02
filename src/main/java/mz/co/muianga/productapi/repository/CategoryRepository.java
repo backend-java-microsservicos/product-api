c373f3e927a7e851874d6720e383e6ff2f5eb784
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2025-02-02 10:26:48 +0200

    chore: create repositories

---
+++ src/main/java/mz/co/muianga/productapi/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package mz.co.muianga.productapi.repository;
+
+import mz.co.muianga.productapi.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
