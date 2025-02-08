87906ad19aee73178e172b76053026867e282522
Nilvandro Muianga <nilvandro.muianga@vm.co.mz>
2025-02-08 14:56:10 +0200

    chore: add exception handler MethodArgumentNotValidException

--- src/main/java/mz/co/muianga/productapi/exception/ProductControllerAdvice.java
+++ src/main/java/mz/co/muianga/productapi/exception/ProductControllerAdvice.java
@@ -4,12 +4,16 @@ import mz.co.muianga.shoppingclient.dto.ErrorDTO;
 import mz.co.muianga.shoppingclient.exception.CategoryNotFoundException;
 import mz.co.muianga.shoppingclient.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Date;
+import java.util.List;
 
 @ControllerAdvice
 public class ProductControllerAdvice {
@@ -35,6 +39,25 @@ public class ProductControllerAdvice {
         errorDTO.setStatus(HttpStatus.NOT_FOUND.value());
         errorDTO.setTimestamp(new Date());
 
+        return errorDTO;
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorDTO processValidationError(MethodArgumentNotValidException ex) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(HttpStatus.BAD_REQUEST.value());
+        BindingResult result = ex.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        StringBuilder sb =
+                new StringBuilder("Valor inválido para o(s) campo(s):");
+        for (FieldError fieldError : fieldErrors) {
+            sb.append(" ");
+            sb.append(fieldError.getField());
+        }
+        errorDTO.setMessage(sb.toString());
+        errorDTO.setTimestamp(new Date());
         return errorDTO;
     }
 }
